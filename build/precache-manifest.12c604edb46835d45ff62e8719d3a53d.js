ebb5d9565ae0284ae634ff926beb32549550d857
van-yog <ikharchenko2008@gmail.com>
2021-01-10 12:09:28 +0300

    add production build

---
+++ build/precache-manifest.12c604edb46835d45ff62e8719d3a53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f4b5e674db455a7b85fcd2f9fa0360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ab9d1347931284ad0e",
+    "url": "/static/css/main.7a23d581.chunk.css"
+  },
+  {
+    "revision": "897e114a097c47ade1f2",
+    "url": "/static/js/2.754c6203.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.754c6203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ab9d1347931284ad0e",
+    "url": "/static/js/main.5d10b9ed.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
